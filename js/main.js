bf9d469dfa42ed943789100c90ac45e20d9bd76d
daniyani <daniyanii@yandex.ru>
2021-05-10 21:39:57 +0300

    Fixed: added keyboard switching

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
+keyboard: {
+  enabled: true,
+  onlyInViewport: true,
   
+}  
 
 });
