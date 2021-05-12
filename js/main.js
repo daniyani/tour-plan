5b9a88929a3fb7e3e43e9f04e2d242c33048fdaa
daniyani <daniyanii@yandex.ru>
2021-05-12 20:48:46 +0300

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ keyboard: {
 }  
 
 });
+
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+ 
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+keyboard: {
+  enabled: true,
+  onlyInViewport: true,
+  
+}  
+
+});
